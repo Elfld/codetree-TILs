879365d7a4cbb84914e81cb950afd8e35f85d6ce
Elfld <133224209+Elfld@users.noreply.github.com>
2024-07-28 17:38:31 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> permutation;
+
+bool duplicated(){
+    for(int length=2;length<=(permutation.size()-1)/2;length++){
+        for(int j=1;j<=permutation.size()-length*2;j++){
+            int k=0;
+            for(;k<length;k++){
+                if(permutation.at(j+k)!=permutation.at(j+k+length)) break;
+            }
+            if (k==length) return true;
+        }
+    }
+    return false;
+}
+
+
+void lining(int count){
+    if(count==n){
+        for(int i=1;i<permutation.size();i++){
+            cout<<permutation.at(i);
+        }
+        return;
+    }
+
+    for(int i=4;i<=6;i++){
+        if(permutation.back()==i) continue; 
+        permutation.push_back(i);
+        if(duplicated()) {
+            permutation.pop_back();
+            continue;
+        }
+        lining(++count);
+    }
+
+}
+
+int main() {
+    cin>>n;
+    
+    permutation.push_back(0);
+    
+    lining(0);
+
+    return 0;
+
+}
