fe193b8ce79147015a22f56be81e332fe942038f
Elfld <133224209+Elfld@users.noreply.github.com>
2024-07-28 18:44:07 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
+++ 240728/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int n;
+bool printed {false};
 vector<int> permutation;
 
 bool duplicated(){
@@ -19,23 +20,26 @@ bool duplicated(){
 }
 
 
-void lining(){
-    if(permutation.size()==n+1){
+void lining(int count){
+    if(count==n){
         for(int i=1;i<permutation.size();i++){
             cout<<permutation.at(i);
         }
+        printed=true;
         return;
     }
 
-    for(int i=4;i<=6;i++){
-        if(permutation.back()==i) continue; 
+    for(int i=4;i<=6&&!printed;i++){
+        if(permutation.back()==i) continue;
         permutation.push_back(i);
         if(duplicated()) {
             permutation.pop_back();
             continue;
         }
-        lining();
+        lining(count+1);
+        permutation.pop_back();
     }
+
 }
 
 int main() {
@@ -43,7 +47,7 @@ int main() {
     
     permutation.push_back(0);
     
-    lining();
+    lining(0);
 
     return 0;
 
