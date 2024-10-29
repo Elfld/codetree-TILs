9db2d81b2e864d0651f4590468bce4c8a3df2610
Elfld <133224209+Elfld@users.noreply.github.com>
2024-10-29 15:03:50 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241029/자연수 n개의 합/sum-of-n-natural-numbers.cpp
+++ 241029/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -6,10 +6,14 @@ int main() {
     cin>>s;
 
     long long left=1;
-    long long right=s;
+    long long right=2000000000;
+    long long maxi = 0;
     while(left<=right){
         long long mid = (left + right)/2;
-        if((mid*mid+1)/2 < s) left = mid+1;
+        if(mid*(mid+1)/2 < s) {
+            left = mid+1;
+            maxi=mid;
+        }
         else right = mid - 1;
     }
     cout<<right;
