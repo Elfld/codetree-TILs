88ab26a8c94a5564ad6dbc71bb784bafed397c07
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-08 11:17:27 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250108/자연수 n개의 합/sum-of-n-natural-numbers.cpp
+++ 250108/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
     long long s,n;
     cin>>s;
-    long long left=0,right=s;
+    long long left=1,right=s;
+    long long maxi{0};
     while(left<=right){
         long long mid = (left+right)/2;
-        long long cs = (mid+1)*mid/2;
-
-        if(cs >= s){
+        long long cs = (mid+1)*mid/2.0;
+        
+        if(cs > s){
             right = mid-1;
         }
+        else if (cs == s){
+            maxi = mid;
+            break;
+        }
         else {
+            maxi=max(maxi,mid);
             left = mid+1;
         }
+        
     }
     cout<<right;
     return 0;
