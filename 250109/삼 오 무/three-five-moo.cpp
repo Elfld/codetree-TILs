78ed51df42cc089a0104b71d95f5e7529e7ddff7
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-09 11:36:06 +0900

    Title: 삼 오 무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250109/삼 오 무/three-five-moo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int howmoo(int k){
+    int moo3 = k/3;
+    int moo5 = k/5;
+    int moo15 = k/15;
+
+    return moo3+moo5-moo15;
+}
+
+int main() {
+    long long N;
+    cin>>N;
+
+    int left = 1, right = min(1LL<<30,3LL*N);
+
+    while(left <= right){
+        int mid = (left+right)/2;
+        int res = mid-howmoo(mid);
+        if(res < N){
+            left = mid + 1;
+        }
+        else if (res == N){
+            cout <<mid;
+            return 0;
+        } 
+        else {
+            right = mid-1;
+        }
+    }
+
+
+    return 0;
+}
