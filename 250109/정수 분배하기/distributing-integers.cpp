87a56a184e378435bd7a7e10673b96de131f330a
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-09 11:14:03 +0900

    Title: 정수 분배하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250109/정수 분배하기/distributing-integers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m, total{0};
+    cin>>n>>m;
+    vector<int> ls;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        ls.push_back(a);
+        total+=a;
+    }
+
+    int left{0}, right{total}, mx{0};
+
+    while(left<=right){
+        int mid = (left+right)/2;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(ls[i]<mid) {
+                right = mid-1;
+                continue;
+            }
+            count += ls[i]/mid;
+        }
+
+        if(count<m) {
+            right = mid - 1;
+        }
+        else {
+            left = mid+1;
+            mx = max(mx, mid);
+        }
+    }
+
+    cout<<mx;
+
+    return 0;
+}
