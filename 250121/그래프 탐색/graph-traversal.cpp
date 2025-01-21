24e42338112b59a388c464d4e6cb00470d66784a
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-21 18:58:01 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250121/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int n, m;
+int from[10000], to[10000];
+bool vt[1001];
+int main() {
+    cin >> n >> m;
+    vector<vector<int>> nd(n+1);
+    for (int i = 0; i < m; i++) {
+        cin >> from[i] >> to[i];
+        nd[from[i]].push_back(to[i]);
+    }
+
+    // Write your code here!
+    stack<int> s;
+    s.push(1);
+    int count{-1};
+    while(!s.empty()){
+        int cur = s.top();
+        s.pop();
+        if(vt[cur]) continue;
+        vt[cur] = true;
+        count++;
+        for(int cn : nd[cur]){
+            if(vt[cn])continue;
+            s.push(cn);
+        }
+    }
+
+    cout<<count;
+
+    return 0;
+}
