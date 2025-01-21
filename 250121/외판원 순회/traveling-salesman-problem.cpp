9aa41969a638601d50b2972f6f17ec586e745079
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-21 18:26:18 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250121/외판원 순회/traveling-salesman-problem.cpp
+++ 250121/외판원 순회/traveling-salesman-problem.cpp
@@ -24,8 +24,11 @@ void qq(int k){
         od.push_back(0);
         int ct{0};
         for(int i=0;i<od.size()-1;i++){
-            ct+= A[od[i]][od[i+1]] ;
+            int cur = A[od[i]][od[i+1]];
+            cout << cur<<' ';
+            ct+= cur;
         }
+        cout<<"\n";
         mins = min(mins,ct);
         od.pop_back();
         return;
