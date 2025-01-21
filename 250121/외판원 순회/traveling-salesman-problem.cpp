49d6a206ee30c267062582aa7644e28d4f720de3
Yohan <133224209+Elfld@users.noreply.github.com>
2025-01-21 18:19:42 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250121/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n;
+int A[10][10];
+
+vector<int> od;
+vector<bool> vs;
+
+int mins{INT_MAX};
+
+bool ca(int a, int b){
+    if(A[a][b]==0){
+        return false;
+    }
+    return true;
+}
+
+void qq(int k){
+    if(k==n-1){
+        od.push_back(0);
+        int ct{0};
+        for(int i=0;i<od.size()-1;i++){
+            ct+=A[od[i]][od[i+1]];
+        }
+        mins = min(mins,ct);
+        od.pop_back();
+        return;
+    }
+
+    for(int i=1;i<n;i++){
+        if(!vs[i]){
+            vs[i]=true;
+            od.push_back(i);
+            qq(k+1);
+            od.pop_back();
+            vs[i]=false;
+        }
+    }
+}
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Write your code here!
+    vs.resize(n,false);
+    od.push_back(0);
+
+    qq(0);
+
+    cout<<mins;
+    return 0;
+}
